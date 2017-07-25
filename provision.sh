05683b67defdc3d43f9e582695063d20490bd52b
Mathieu Dähne <mathieudahne@Mathieus-MacBook-Pro.local>
2017-07-25 20:45:15 +0100

    initial commit

---
+++ provision.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+SRC_DIRECTORY="$HOME/dev/provision"
+ANSIBLE_DIRECTORY="$SRC_DIRECTORY/ansible"
+ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible.d"
+
+# Download and install Command Line Tools
+if [[ ! -x /usr/bin/gcc ]]; then
+    echo "Info   | Install   | xcode"
+    xcode-select --install
+fi
+
+# Download and install Homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Modify the PATH
+# export PATH=/usr/local/bin:$PATH
+
+# Download and install git
+if [[ ! -x /usr/local/bin/git ]]; then
+    echo "Info   | Install   | git"
+    brew install git
+fi
+
+# Download and install python
+if [[ ! -x /usr/local/bin/python ]]; then
+    echo "Info   | Install   | python"
+    brew install python --framework --with-brewed-openssl
+fi
+
+# Download and install Ansible
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    brew install ansible
+fi
+
+# Clone repository
+if [[ ! -x SRC_DIRECTORY ]]; then
+    git clone git@github.com:mathieud/mac-provision.git $SRC_DIRECTORY
+fi
+
+
+
+# Make the code directory
+mkdir -p $SRC_DIRECTORY
+
+# FIX ANSIBLE PERMISSIONS
+chmod -R 750 $SRC_DIRECTORY
+chmod -R 660 $ANSIBLE_DIRECTORY/inventories/*
+
+# Provision the box
+# ansible-playbook -i $ANSIBLE_DIRECTORY/inventories/osx.ini $ANSIBLE_DIRECTORY/playbook.yml --connection=local
+ansible-playbook --ask-sudo-pass -i $ANSIBLE_DIRECTORY/inventories/osx.ini $ANSIBLE_DIRECTORY/playbook.yml --connection=local
